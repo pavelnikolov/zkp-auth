ee1c988794b52fde544d04b63916a3590c4d161c
Pavel Nikolov <me@pavelnikolov.net>
2024-07-26 00:08:25 +0300

    update terraform and add k8s files
    Signed-off-by: Pavel Nikolov <me@pavelnikolov.net>

--- terraform/main.tf
+++ terraform/main.tf
@@ -16,14 +16,24 @@ provider "aws" {
   region = var.aws_region
 }
 
-
 resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
 }
 
-resource "aws_subnet" "main" {
-  vpc_id     = aws_vpc.main.id
-  cidr_block = "10.0.1.0/24"
+resource "aws_subnet" "az_a" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = format("%s%s", var.aws_region, "a")
+
+  tags = {
+    Name = "main"
+  }
+}
+
+resource "aws_subnet" "az_b" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.2.0/24"
+  availability_zone = format("%s%s", var.aws_region, "b")
 
   tags = {
     Name = "main"
@@ -53,8 +63,32 @@ resource "aws_iam_role_policy_attachment" "cluster_policy" {
   role       = aws_iam_role.cluster_role.name
 }
 
-# Optionally, enable Security Groups for Pods
-# Reference: https://docs.aws.amazon.com/eks/latest/userguide/security-groups-for-pods.html
+resource "aws_iam_role" "NodeGroupRole" {
+  name = "EKSNodeGroupRole"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      },
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "AmazonEKS_CNI_Policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+  role       = aws_iam_role.NodeGroupRole.name
+}
+
+resource "aws_iam_role_policy_attachment" "AmazonEKSWorkerNodePolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+  role       = aws_iam_role.NodeGroupRole.name
+}
+
 resource "aws_iam_role_policy_attachment" "vpc_resource_controller_policy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSVPCResourceController"
   role       = aws_iam_role.cluster_role.name
@@ -65,7 +99,7 @@ resource "aws_eks_cluster" "default" {
   role_arn = aws_iam_role.cluster_role.arn
 
   vpc_config {
-    subnet_ids = [aws_subnet.main.id]
+    subnet_ids = [aws_subnet.az_a.id, aws_subnet.az_b.id]
   }
 
   # Ensure that IAM Role permissions are created before and deleted after EKS Cluster handling.
@@ -74,4 +108,36 @@ resource "aws_eks_cluster" "default" {
     aws_iam_role_policy_attachment.cluster_policy,
     aws_iam_role_policy_attachment.vpc_resource_controller_policy,
   ]
+}
+
+resource "aws_eks_node_group" "cluster_node_group" {
+  cluster_name    = aws_eks_cluster.default.name
+  node_group_name = "${terraform.workspace}-cluster-node_group"
+  node_role_arn   = aws_iam_role.NodeGroupRole.arn
+  subnet_ids      = [aws_subnet.az_a.id, aws_subnet.az_b.id]
+
+  scaling_config {
+    desired_size = 2
+    max_size     = 2
+    min_size     = 2
+  }
+
+  ami_type       = "AL2_x86_64"
+  instance_types = ["t3.micro"]
+  capacity_type  = "ON_DEMAND"
+  disk_size      = 20
+
+  depends_on = [
+    aws_iam_role_policy_attachment.AmazonEKSWorkerNodePolicy,
+    aws_iam_role_policy_attachment.AmazonEKS_CNI_Policy
+  ]
+}
+
+# use managed addons in order to make it easier to upgrade the Kubernetes version in future
+resource "aws_eks_addon" "addons" {
+  for_each                 = { for addon in var.cluster_addons : addon.name => addon }
+  cluster_name             = aws_eks_cluster.default.name
+  addon_name               = each.value.name
+  addon_version            = each.value.version
+  service_account_role_arn = aws_iam_role.cluster_role.arn
 }
