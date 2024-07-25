fd3831e95af894dbc76e4c7125ae7295f0787043
Pavel Nikolov <me@pavelnikolov.net>
2024-07-26 00:08:25 +0300

    update terraform and add k8s files
    Signed-off-by: Pavel Nikolov <me@pavelnikolov.net>

--- terraform/output.tf
+++ terraform/output.tf
@@ -1,7 +1,9 @@
 output "endpoint" {
-  value = aws_eks_cluster.default.endpoint
+  value     = aws_eks_cluster.default.endpoint
+  sensitive = true
 }
 
 output "kubeconfig-certificate-authority-data" {
-  value = aws_eks_cluster.default.certificate_authority[0].data
+  value     = aws_eks_cluster.default.certificate_authority[0].data
+  sensitive = true
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -3,3 +3,25 @@ variable "aws_region" {
   default     = "eu-central-1"
   type        = string
 }
+
+variable "cluster_addons" {
+  type = list(object({
+    name    = string
+    version = string
+  }))
+
+  default = [
+    {
+      name    = "kube-proxy"
+      version = "v1.30.0-eksbuild.3"
+    },
+    {
+      name    = "vpc-cni"
+      version = "v1.18.2-eksbuild.1"
+    },
+    {
+      name    = "coredns"
+      version = "v1.11.1-eksbuild.9"
+    }
+  ]
+}
