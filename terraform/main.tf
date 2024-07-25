eb5e4675322b72c829ca213e86ff70b68c4a0851
Pavel Nikolov <me@pavelnikolov.net>
2024-07-25 19:20:44 +0300

    set region
    Signed-off-by: Pavel Nikolov <me@pavelnikolov.net>

--- terraform/main.tf
+++ terraform/main.tf
@@ -9,8 +9,11 @@ terraform {
   backend "s3" {
     bucket = var.state_bucket
     key    = "terraform/eks/terraform.tfstate"
-    region = var.aws_region
   }
+}
+
+provider "aws" {
+  region = var.aws_region
 }
 
 
